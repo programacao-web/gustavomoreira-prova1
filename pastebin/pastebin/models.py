245fe6c21a9da20495b3fc0885ab4a3eda15ef89
Gustavo Moreira <gustavomn93@gmail.com>
2018-05-10 18:55:57 -0300

    Commitao

--- pastebin/pastebin/models.py
+++ pastebin/pastebin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Paste(models.Model):
+    linguagens = (
+        ('Python','Python'),
+        ('Javascript','Javascript'),
+        ('Outros','Outros'),
+    )
+    titulo = models.CharField(max_length=200)
+    linguagem = models.CharField(max_length=11, choices=linguagens)
+    conteudo = models.TextField()
--- pastebin/pastebin/views.py
+++ pastebin/pastebin/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.utils import timezone
+from .models import Paste
+from .forms import PasteForm
+from django.shortcuts import redirect
 
 def index(request):
-    ctx = {}
+    if request.method == "POST":
+        form = PasteForm(request.POST)
+        if form.is_valid():
+           form.save()
+    else:
+        form = PasteForm()
+
+    ctx = {'form': form}
     return render(request, 'pastebin/index.jinja2', ctx)
 
 
 def paste(request, id):
-    ctx = {}
+    paste = get_object_or_404(Paste, pk=id)
+    ctx = {"paste": paste}
     return render(request, 'pastebin/paste-detail.jinja2', ctx)
 
 
 def language_list(request, language):
     ctx = {'pastes': []}
     return render(request, 'pastebin/paste-language.jinja2', ctx)
+
+def paste_new(request):
+    if request.method == "POST":
+        form = PasteForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PasteForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
